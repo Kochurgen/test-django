5b98056374fccadae80f0272aabeddd889f35f6c
kochurgen <kochurgen@gmail.com>
2018-05-31 08:42:54 +0300

    urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf.urls import include, url
+from django.conf.urls import  url
 from django.contrib import admin
+from blog import urls
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
-    url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', urls),
+    url(r'^admin/', admin.site.urls),
 ]
