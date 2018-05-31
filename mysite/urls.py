3748dedb8997d6996a96a8047966f435fab4b897
kochurgen <kochurgen@gmail.com>
2018-05-31 10:26:04 +0300

    urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
-    url(r'^/', include("blog.urls")),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include("blog.urls")),
+    url(r'^admin/', admin.site.urls),
 ]
